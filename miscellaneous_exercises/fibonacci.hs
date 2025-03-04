823672a3983b2b42d61fad42388326d5f484f85f
Adam Jam <adamjamrozinski02@gmail.com>
2025-03-04 01:23:17 +0100

    fibonacci benchmark

--- miscellaneous_exercises/fibonacci.hs
+++ miscellaneous_exercises/fibonacci.hs
@@ -1,15 +1,18 @@
-main = do
-    putStrLn "hello"
-    putStrLn ("please look: " ++ show (filter odd [10..20]))
-    print (fact01 5)
+import Criterion.Main
 
+main :: IO ()
+main = defaultMain [
+            bgroup "fib" [
+                bench "fib1" $ whnf fib 25,
+                bench "fib2" $ whnf (\n -> fib2 !! n) 25
+            ]
+        ]
 
-fact01 n = if n==0 then 1 else fact01 (n-1)
-
-fact02 0 = 1
-fact02 1 = 1
-fact02 n = n * fact02 (n-2) 
-
+fib :: Int -> Int
 fib 0 = 1
 fib 1 = 1
 fib n = fib(n-1) + fib(n-2)
+
+fib2 = 1 : 1 : [ a+b | (a,b) <- zip fib2 (tail fib2) ]
+
+fib3 = 0:1: zipWith (+) fib3 (tail fib3)
